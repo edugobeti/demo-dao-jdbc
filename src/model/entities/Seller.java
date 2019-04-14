271d77a77f180c16714125e18636714c2dea8462
edugobeti <edugobeti@gmail.com>
2019-04-14 16:06:59 -0300

    Adicionado o insert

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -17,8 +17,7 @@ public class Seller implements Serializable{
 	public Seller() {
 	}
 
-	public Seller(int id, String name, String email, Date birthDate, Double baseSalary, Department department) {
-		super();
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
