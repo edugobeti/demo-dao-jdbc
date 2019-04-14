1b0b7d3ac2c814d0de574a9fe5e38df31fd3b93c
edugobeti <edugobeti@gmail.com>
2019-04-14 17:02:18 -0300

    Adicionado deleteById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
 
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===Teste 1 findById===");
@@ -43,6 +43,14 @@ public class Program {
 		seller.setName("Martha Stwart");
 		sellerDao.update(seller);
 		System.out.println("Update completed! ");
+		
+	
+		System.out.println("===\nTeste 5 deleteById===");
+		System.out.println("Insira um id para teste de deleção: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed! ");
+		sc.close();
 		*/
 	}
 
