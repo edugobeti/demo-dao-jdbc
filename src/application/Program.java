31ca2a3ec403e7a9e6073961aaf89cc1d6058c0c
edugobeti <edugobeti@gmail.com>
2019-04-14 14:52:21 -0300

    Adicionado FindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,10 +13,17 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== Teste N1 find by Id===");
+		System.out.println("===Teste 1 find by Id===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+
+		System.out.println("\n===Teste find by Department===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj : list ) {
+			System.out.println(obj);
+		}
 	}
 
 }
