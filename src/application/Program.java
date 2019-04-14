e876416d853e4bc48c570a2c48f8fe48215f8b46
edugobeti <edugobeti@gmail.com>
2019-04-13 21:29:10 -0300

    Reutização da instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== Teste N1 find by Id===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
